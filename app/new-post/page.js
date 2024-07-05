86cfb3c806c6575db97369bc36a3631f6c19ffce
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-05 09:36:12 +0300

    feat: Add form submission component and update create post functionality
    The code changes include adding a new file, "form-submit.js", which contains the implementation of a form submission component. This component will be used in the "NewPostPage" component. Additionally, the "createPost" function in the "actions.js" file is updated to log the form data and store the post using the "storePost" function. After storing the post, the page is redirected to the "/feed" route.
    
    This commit introduces a new feature and improves the functionality of creating posts.

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,33 +1,22 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import FormSubmit from "@/components/form-submit";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
+import { createPost } from "@/lib/actions";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    console.log(title, image, content);
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
+  const [state, formAction] = useFormState(createPost, {});
 
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
+          {state.errors && state.errors.title && (
+            <p className="error">{state.errors.title}</p>
+          )}
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
@@ -37,10 +26,16 @@ export default function NewPostPage() {
             id="image"
             name="image"
           />
+          {state.errors && state.errors.image && (
+            <p className="error">{state.errors.image}</p>
+          )}
         </p>
         <p className="form-control">
           <label htmlFor="content">Content</label>
           <textarea id="content" name="content" rows="5" />
+          {state.errors && state.errors.content && (
+            <p className="error">{state.errors.content}</p>
+          )}
         </p>
         <p className="form-actions">
           <FormSubmit />
--- components/form-submit.js
+++ components/form-submit.js
@@ -1,5 +1,5 @@
 "use client";
-
+import { useFormStatus } from "react-dom";
 const FormSubmit = () => {
   const status = useFormStatus();
 
