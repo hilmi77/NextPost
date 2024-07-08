f915dd8da37e96085ef64304b340728b02720601
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-08 13:07:15 +0300

    chore: Update form submission component and post like functionality
    The code changes include updating the "Post" component to include a form for toggling the like status of a post. The "Post" component now accepts an "action" prop which is used to update the like status of a post. Additionally, the "Posts" component is modified to use the "useOptimistic" hook to handle optimistic updates when toggling the like status of a post. The "togglePostStatus" function in the "actions.js" file is added to handle the API call for updating the like status of a post.
    
    This commit improves the functionality of the post like feature and updates the form submission component.

--- components/posts.js
+++ components/posts.js
@@ -1,7 +1,11 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+"use client";
 
-function Post({ post }) {
+import { useOptimistic } from "react";
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostStatus } from "@/lib/actions";
+
+function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
@@ -12,14 +16,19 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form
+              action={action.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
@@ -29,15 +38,38 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPosts, updatedPostId) => {
+      const updatedPostIndex = prevPosts.findIndex(
+        (post) => post.id === updatedPostId
+      );
+
+      if (updatedPostIndex === -1) {
+        return prevPosts;
+      }
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+      return newPosts;
+    }
+  );
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
+  }
+
+  async function updatedPost(postId) {
+    updateOptimisticPosts(postId);
+    await togglePostStatus(postId);
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatedPost} />
         </li>
       ))}
     </ul>
