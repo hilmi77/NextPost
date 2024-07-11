30b80126dc9686b91a75b226873998618d6a668d
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-11 12:01:13 +0300

    Improve page load performance with lazy loading for images

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,5 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
 
 export default async function FeedPage() {
   const posts = await getPosts();
