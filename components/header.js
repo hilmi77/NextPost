0d01cce92571d6e315c4d423cd2fb68e3c1b27f2
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-11 11:59:56 +0300

    feat: Improve header component with lazy loading for logo image
    The code changes include replacing the `<img>` tag with the `<Image>` component from Next.js for the logo image in the header. This change enables lazy loading for the logo image, improving the page load performance. The `priority` prop is added to ensure the logo image is loaded with high priority.
    
    This commit enhances the header component by implementing lazy loading for the logo image.

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
-import logo from '@/assets/logo.png';
-import Link from 'next/link';
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
+  console.log("Header component rendered", logo);
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image src={logo} alt="Mobile phone with posts feed on it" priority />
       </Link>
       <nav>
         <ul>
@@ -13,7 +15,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
